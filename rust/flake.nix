d5e128f682d375d6cde5cf89138823622bb3db82
Michael Forster <email@michael-forster.de>
2022-11-17 12:55:19 +0100

    Add a Rust development environment

---
+++ rust/flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Mike's Rust development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+    rust-overlay.inputs.flake-utils.follows = "flake-utils";
+  };
+
+  outputs = { nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+      in with pkgs; {
+        devShells.default = mkShell {
+          buildInputs = [
+            (rust-bin.stable.latest.default.override {
+              extensions = [ "rust-src" ];
+            })
+            cargo-audit
+            cargo-edit
+            cargo-release
+            cargo-watch
+          ];
+        };
+      });
+}
